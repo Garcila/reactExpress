e12c6e91c24aef1afcacdeb8f141d8edee6a22ec
German Arcila <germanarcila@gmail.com>
2017-08-03 22:24:48 -0400

    added mlab account to be managed with mongoose and also created a config/keys file containing for now jus the key to mlab

--- app.js
+++ app.js
@@ -23,8 +23,10 @@ const keys = require('./config/keys');
 //bluebird promises to replace default mongoose promises
 mongoose.Promise = require('bluebird');
 
+//connection to mlab mongodb
 mongoose.connect(keys.mongoURI);
 
+//connection to local mongo db
 // mongoose.connect('mongodb://localhost/faces');
 
 // view engine setup
@@ -43,23 +45,23 @@ app.use('/users', users);
 app.use(images);
 app.use('/profile', profile);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use((err, req, res) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 // module.exports = app;
 
