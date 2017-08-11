a60db6b2c49e85c4ddc67a9754364c7d7900ab15
German Arcila <germanarcila@gmail.com>
2017-08-10 21:10:27 -0400

    keys db configuration

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const profile = require('./routes/profile');
 const mongoose = require('mongoose');
 
 //file with keys not to be saved to git
-const keys = require('./config/keys');
+// const keys = require('./config/keys');
 
 //bluebird promises to replace default mongoose promises
 mongoose.Promise = require('bluebird');
