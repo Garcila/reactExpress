fb794934bd1301b1971fa7611be84d3c0793460d
German Arcila <germanarcila@gmail.com>
2018-03-30 22:08:28 -0400

    from superuser to netlify

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -3,6 +3,6 @@ export const AUTH_CONFIG = {
     clientID: 'bhbJ5gRzFA9BpueCl4NFVUlYwwLv9x14',
     redirectUri: 'https://1000faces.netlify.com/',
     // redirectUri: 'http://localhost:3000/Superuser',
-    redirectUri: 'http://localhost:3000/',
+    // redirectUri: 'http://localhost:3000/',
     audience: 'https://germanarcila.auth0.com/userinfo',
 }
